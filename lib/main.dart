3c226c5ae97358df480a9d1212ebcb7e75538134
Curtis Baker <bakercurtis143@gmail.com>
2026-05-25 13:46:41 +0100

    Add visible iOS error screen for TestFlight debugging

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,63 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import 'features/home/home_page.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const BrainAgeApp());
+  runZonedGuarded(() {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FlutterError.presentError(details);
+    };
+
+    ErrorWidget.builder = (FlutterErrorDetails details) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: const Color(0xFF0B1020),
+          body: SafeArea(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(18),
+              child: Text(
+                details.exceptionAsString(),
+                style: const TextStyle(
+                  color: Colors.redAccent,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    };
+
+    runApp(const BrainAgeApp());
+  }, (Object error, StackTrace stackTrace) {
+    runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: const Color(0xFF0B1020),
+          body: SafeArea(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(18),
+              child: Text(
+                '$error\n\n$stackTrace',
+                style: const TextStyle(
+                  color: Colors.redAccent,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  });
 }
 
 class BrainAgeApp extends StatelessWidget {
