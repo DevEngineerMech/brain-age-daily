7c139bbb832d8dc5994ca96182d5af5a93a78ec3
Curtis Baker <bakercurtis143@gmail.com>
2026-05-29 18:46:52 +0100

    Re-enable AdMob init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'features/home/home_page.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await MobileAds.instance.initialize();
+
   runApp(const BrainAgeDailyApp());
 }
 
